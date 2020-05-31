aa7aff93be550846201b31d205430a56bf6dff07
dannyflatiron <dannyflatiron@gmail.com>
2020-05-31 03:19:38 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet (time) {
+  const words = time.split(':')
+  let first_word = words[0]
+  let first_number = parseInt(first_word, 10)
+  if (first_number < 12) return "Good Morning"
+  if (first_number > 17) return "Good Evening"
+  return "Good Afternoon"
+
+}
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string
+}
