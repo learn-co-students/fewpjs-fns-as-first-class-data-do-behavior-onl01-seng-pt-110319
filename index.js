6d90d904f7e3e0a25f4d9945b2ca7a7d98e9ca3f
Wills Blake <richwblake@gmail.com>
2020-06-07 18:33:05 -0400

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,27 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function changeTimeStringToInt (timeString) {
+  let convertedString = timeString.split(':');
+  convertedString = convertedString.join('');
+  return Number.parseInt(convertedString);
+}
+
 /* Write your implementation of greet() */
+
+function greet (timeString) {
+  const time = changeTimeStringToInt(timeString);
+  if (time < 1200) {
+    return 'Good Morning';
+  } else if (time > 1200 && time < 1700) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Evening';
+  }
+}
+
+function displayMessage (greetString) {
+  document.getElementById('greeting').innerText = greetString;
+}
+
 /* Write your implementation of displayMessage() */
